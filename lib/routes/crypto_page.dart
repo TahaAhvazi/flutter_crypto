7fef5ff5252dd667a4589f0eaba4e462f14a7e4c
Taha <tahax.dev@gmail.com>
2022-08-04 01:13:27 +0430

    Add an refresh indicator

--- lib/routes/crypto_page.dart
+++ lib/routes/crypto_page.dart
@@ -8,9 +8,14 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class CryptoPage extends StatelessWidget {
+class CryptoPage extends StatefulWidget {
   const CryptoPage({Key? key}) : super(key: key);
 
+  @override
+  State<CryptoPage> createState() => _CryptoPageState();
+}
+
+class _CryptoPageState extends State<CryptoPage> {
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
@@ -65,34 +70,44 @@ class CryptoPage extends StatelessWidget {
                           ],
                         ),
                       ),
-                      Container(
+                      SizedBox(
                         width: width,
                         height: height * 80 / 100,
-                        child: ListView.builder(
-                          itemCount: state.cryptoList.length,
-                          itemBuilder: (context, index) {
-                            return Column(
-                              children: [
-                                const Divider(
-                                  color: Colors.black,
-                                ),
-                                GestureDetector(
-                                  onTap: () => Navigator.of(context)
-                                      .push(MaterialPageRoute(
-                                    builder: (context) => const CryptoDetail(),
-                                  )),
-                                  child: CryptoItem(
-                                    imagesrc: state.cryptoList[index].image,
-                                    cryptoName: state.cryptoList[index].name,
-                                    cryptoLastPrice:
-                                        state.cryptoList[index].currentPrice,
-                                    cryptochangePrice24: state.cryptoList[index]
-                                        .priceChangePercentage24H,
-                                  ),
-                                ),
-                              ],
-                            );
+                        child: RefreshIndicator(
+                          color: Colors.green,
+                          onRefresh: () async {
+                            await Future.delayed(
+                                const Duration(milliseconds: 1500));
+                            setState(() {});
                           },
+                          child: ListView.builder(
+                            itemCount: state.cryptoList.length,
+                            itemBuilder: (context, index) {
+                              return Column(
+                                children: [
+                                  const Divider(
+                                    color: Colors.black,
+                                  ),
+                                  GestureDetector(
+                                    onTap: () => Navigator.of(context)
+                                        .push(MaterialPageRoute(
+                                      builder: (context) =>
+                                          const CryptoDetail(),
+                                    )),
+                                    child: CryptoItem(
+                                      imagesrc: state.cryptoList[index].image,
+                                      cryptoName: state.cryptoList[index].name,
+                                      cryptoLastPrice:
+                                          state.cryptoList[index].currentPrice,
+                                      cryptochangePrice24: state
+                                          .cryptoList[index]
+                                          .priceChangePercentage24H,
+                                    ),
+                                  ),
+                                ],
+                              );
+                            },
+                          ),
                         ),
                       )
                     ],
